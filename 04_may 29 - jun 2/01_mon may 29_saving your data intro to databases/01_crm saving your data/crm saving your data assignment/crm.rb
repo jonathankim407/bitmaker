ce0e9e2b75fb81ea7661761cd7059d816ec2722a
jonathankim407 <jonathankim407@gmail.com>
2017-05-30 20:16:33 -0400

    2. Creating a Contact

--- 04_may 29 - jun 2/01_mon may 29_saving your data intro to databases/01_crm saving your data/crm saving your data assignment/crm.rb
+++ 04_may 29 - jun 2/01_mon may 29_saving your data intro to databases/01_crm saving your data/crm saving your data assignment/crm.rb
@@ -54,7 +54,13 @@ class CRM
     print 'Enter a Note: '
     note = gets.chomp
 
-    Contact.create(first_name, last_name, email, note)
+    # Contact.create(first_name, last_name, email, note)
+    contact = Contact.create(
+      first_name: first_name,
+      last_name:  last_name,
+      email:      email,
+      note:       note
+    )
   end
 
   def modify_existing_contact
