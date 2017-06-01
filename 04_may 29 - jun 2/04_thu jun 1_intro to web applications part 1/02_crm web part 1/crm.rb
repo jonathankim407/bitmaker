969e56af31ff45d8c8ac8b7637c51ccb184e99b8
jonathankim407 <jonathankim407@gmail.com>
2017-06-01 15:54:15 -0400

    create home page

---
+++ 04_may 29 - jun 2/04_thu jun 1_intro to web applications part 1/02_crm web part 1/crm.rb
@@ -0,0 +1,158 @@
+# IN TERMINAL USE bundle exec ruby crm.rb
+
+require_relative 'contact'
+require 'sinatra'
+
+get '/' do
+  erb :index
+end
+# http://localhost:4567/
+
+
+
+
+
+
+
+#
+# class CRM
+#
+#   def initialize
+#
+#   end
+#
+#   def main_menu
+#     while true
+#       print_main_menu
+#       user_selected = gets.to_i
+#       call_option(user_selected)
+#     end
+#   end
+#
+#   def print_main_menu
+#     puts '[1] Add a new contact'
+#     puts '[2] Modify an existing contact'
+#     puts '[3] Delete a contact'
+#     puts '[4] Display all the contacts'
+#     puts '[5] Search by attribute'
+#     puts '[6] Exit'
+#     puts 'Enter a number: '
+#   end
+#
+#   def call_option(user_selected)
+#     case user_selected
+#     when 1
+#       add_new_contact
+#     when 2
+#       modify_existing_contact
+#     when 3
+#       delete_contact
+#     when 4
+#       display_all_contacts
+#     when 5
+#       search_by_attribute
+#     when 6
+#       abort("Goodbye!")
+#     end
+#   end
+#
+#   def add_new_contact
+#     print 'Enter First Name: '
+#     first_name = gets.chomp
+#
+#     print 'Enter Last Name: '
+#     last_name = gets.chomp
+#
+#     print 'Enter Email Address: '
+#     email = gets.chomp
+#
+#     print 'Enter a Note: '
+#     note = gets.chomp
+#
+#     # Contact.create(first_name, last_name, email, note)
+#     contact = Contact.create(
+#       first_name: first_name,
+#       last_name:  last_name,
+#       email:      email,
+#       note:       note
+#     )
+#   end
+#
+#   def modify_existing_contact
+#     puts "Please enter the ID of the contact you wish to modify"
+#     user_id = gets.to_i
+#
+#     if Contact.find(user_id) == false
+#       puts "Contact not found!"
+#     else
+#
+#     puts "What do you want to modify?"
+#     puts '[1] First name'
+#     puts '[2] Last name'
+#     puts '[3] Email'
+#     puts '[4] Note'
+#     number_attribute = gets.to_i
+#
+#     case number_attribute
+#       when 1
+#         attribute = "first_name"
+#       when 2
+#         attribute = "last_name"
+#       when 3
+#         attribute = "email"
+#       when 4
+#         attribute = "note"
+#     end
+#
+#     puts "What do you want to set it to?"
+#     value = gets.chomp
+#
+#     Contact.find(user_id).update(attribute, value)
+#   end
+#   end
+#
+#   def delete_contact
+#     puts "Please enter the ID of the contact you wish to delete"
+#     user_id = gets.to_i
+#     Contact.find(user_id).delete
+#   end
+#
+#   def display_all_contacts
+#     Contact.all.each do |contact|
+#       puts "ID: #{contact.id} | Name: #{contact.first_name} #{contact.last_name} | Email: #{contact.email} | Note: #{contact.note}"
+#     end
+#   end
+#
+#   def search_by_attribute
+#     puts "What do you want to search by?"
+#     puts '[1] First name'
+#     puts '[2] Last name'
+#     puts '[3] Email'
+#     puts '[4] Note'
+#     number_attribute = gets.to_i
+#
+#     case number_attribute
+#       when 1
+#         attribute = "first_name"
+#       when 2
+#         attribute = "last_name"
+#       when 3
+#         attribute = "email"
+#       when 4
+#         attribute = "note"
+#     end
+#
+#     puts "What do you want to search"
+#     value = gets.chomp
+#     found_contact = Contact.find_by(attribute, value)
+#       puts "ID: #{found_contact.id} | Name: #{found_contact.first_name} #{found_contact.last_name} | Email: #{found_contact.email} | Note: #{found_contact.note}"
+#   end
+#
+#   a_crm_app = CRM.new
+#   a_crm_app.main_menu
+#
+# end
+#
+# at_exit do
+#   ActiveRecord::Base.connection.close
+# end
