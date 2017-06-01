eef91f14ffe0b563654759c03367033f7734d16c
jonathankim407 <jonathankim407@gmail.com>
2017-06-01 19:14:05 -0400

    1. Create HTML response inline

---
+++ 04_may 29 - jun 2/04_thu jun 1_intro to web applications part 1/03_stretch_assignment_build your own web server/web-server-master/README.md
@@ -0,0 +1,2 @@
+# My First Web Server
+[See instructions in Alexa.](https://alexa.bitmaker.co/cohorts/67/assignments/2029/latest)
---
+++ 04_may 29 - jun 2/04_thu jun 1_intro to web applications part 1/03_stretch_assignment_build your own web server/web-server-master/server.rb
@@ -0,0 +1,35 @@
+require 'socket'                                    # Require socket from Ruby Standard Library (stdlib)
+
+host = 'localhost'
+port = 2000
+
+server = TCPServer.open(host, port)                 # Socket to listen to defined host and port
+puts "Server started on #{host}:#{port} ..."        # Output to stdout that server started
+
+loop do                                             # Server runs forever
+  client = server.accept                            # Wait for a client to connect. Accept returns a TCPSocket
+
+  lines = []
+  while (line = client.gets) && !line.chomp.empty?  # Read the request and collect it until it's empty
+    lines << line.chomp
+  end
+  puts lines                                        # Output the full request to stdout
+
+  header = "HTTP/1.1 200 OK"
+  time = Time.now.ctime
+  response = "
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>My first web server</title>
+      </head>
+      <body>
+        <h1>My first web server</h1>
+        <p>Oh hey, this is my first HTML response!</p>
+      </body>
+    </html>"
+
+  client.puts(response)
+
+  client.close                                      # Disconnect from the client
+end
