a7d12760e199766e4b46946dc94944f229ce359d
jonathankim407 <jonathankim407@gmail.com>
2017-06-05 12:02:58 -0400

    Lecture 20 second half

--- 05_jun 5 - 9/01_mon jun 5_html forms and submitting data/Lecture20/20-html-forms-and-submitting-data/server.rb
+++ 05_jun 5 - 9/01_mon jun 5_html forms and submitting data/Lecture20/20-html-forms-and-submitting-data/server.rb
@@ -7,11 +7,21 @@ get '/' do
 end
 
 get '/films' do
+  @film = Film.find_by(title: params[:title])
+  erb :search_results
+  # "GET request: #{params}"
+end
+
+get '/films/:id' do
   "GET request: #{params}"
 end
 
 post '/films' do
-  "POST request: #{params}"
+  Film.create({title: params[:title], description: params[:description], available: params[:available], rating: params[:rating], country: params[:country]})
+  # erb :index #DON'T DO THIS!
+  redirect to('/')
+  # "POST request: #{params}"
+  # GENERAL RULE: get ENDS WITH erb, post ENDS WITH redirect
 end
 
 put '/films' do
@@ -19,5 +29,8 @@ put '/films' do
 end
 
 delete '/films' do
-  "DELETE request: #{params}"
+  film = Film.find(params[:id])
+  film.delete
+  redirect to('/')
+  # "DELETE request: #{params}"
 end
