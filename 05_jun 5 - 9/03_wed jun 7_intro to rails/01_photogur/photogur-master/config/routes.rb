2687c1269cd5072d5417014dbe1efe7209b932e5
jonathankim407 <jonathankim407@gmail.com>
2017-06-07 13:59:35 -0400

    05/03/01/Adding feature to edit pictures

--- 05_jun 5 - 9/03_wed jun 7_intro to rails/01_photogur/photogur-master/config/routes.rb
+++ 05_jun 5 - 9/03_wed jun 7_intro to rails/01_photogur/photogur-master/config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
   get 'pictures/new' => 'pictures#new' # this is also a new line of code
 
   get 'pictures/:id' => 'pictures#show', as: 'picture'
+
+  get 'pictures/:id/edit' => "pictures#edit", as: "edit_picture"
+  patch 'pictures/:id' => "pictures#update"
+  
 end
